f344a21c61afc8c9e993481e46e6a9c3ac113cac
François Nicolas <francoisnicolas@localhost.localdomain>
2022-02-13 07:45:04 +0100

    evil, auctex, synctex, pdf-tools, no menu bar, no tool bar, fido

---
+++ init.el
@@ -0,0 +1,44 @@
+(setq package-archives
+'(("gnu" . "http://elpa.gnu.org/packages/")
+  ("melpa" . "http://melpa.org/packages/")))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 188 :width normal)))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-save-query nil)
+ '(TeX-source-correlate-method 'synctex)
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(TeX-view-program-selection
+   '(((output-dvi has-no-display-manager)
+      "dvi2tty")
+     ((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "PDF Tools")
+     (output-html "xdg-open")))
+ '(auto-save-timeout 600)
+ '(custom-enabled-themes '(tango-dark))
+ '(desktop-save-mode t)
+ '(menu-bar-mode nil)
+ '(package-selected-packages '(evil auctex pdf-tools))
+ '(tool-bar-mode nil))
+
+
+(require 'evil)
+(evil-mode t)
+
+(require 'pdf-tools)
+(pdf-tools-install)
+
+(fido-mode)
+
