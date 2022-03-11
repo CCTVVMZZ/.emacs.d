2c877b19f696d2bd465f9be8c1bf8a291bd3954e
François Nicolas <francois.prm.nicolas@gmail.com>
2022-03-11 19:40:21 +0100

    installetout pour installer tous les packages automatiquement

---
+++ installetout.el
@@ -0,0 +1,23 @@
+; first, declare repositories
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")))
+
+;; Init the package facility
+;; (require 'package)
+(package-initialize)
+(package-refresh-contents) ;; refreshing packages is time-consuming and should be done on demand
+
+;; Declare packages
+(setq mes-super-packages
+      '(magit
+	evil
+	pdf-tools
+	auctex
+	undo-tree)
+      )
+
+;; Iterate on packages and install missing ones
+(dolist (pkg mes-super-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
