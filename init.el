7ccab3a708d9766344a0f615b67e7bd9a0a2fb22
Charlot Colmes <franicolas@aol.com>
2025-05-25 04:33:58 +0200

    new computer

--- init.el
+++ init.el
@@ -19,16 +19,13 @@
  '(TeX-source-correlate-mode t)
  '(TeX-source-correlate-start-server t)
  '(TeX-view-program-selection
-   '(((output-dvi has-no-display-manager)
-      "dvi2tty")
-     ((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "PDF Tools")
-     (output-html "xdg-open")))
+   '(((output-dvi has-no-display-manager) "dvi2tty")
+     ((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi")
+     (output-pdf "PDF Tools") (output-html "xdg-open")))
  '(auto-save-timeout 600)
  '(column-number-mode t)
  '(coq-compile-before-require t)
+ '(coq-load-path '((rec "/home/francoisnicolas/Documents/coq" "mycoq")))
  '(coq-prog-args '("-emacs"))
  '(coq-use-pg t)
  '(custom-enabled-themes '(tango-dark))
@@ -41,15 +38,28 @@
  '(menu-bar-mode nil)
  '(mouse-wheel-scroll-amount '(2 ((shift) . 1) ((meta)) ((control) . text-scale)))
  '(package-selected-packages
-   '(merlin tuareg rainbow-delimiters company-coq web-mode impatient-mode nodejs-repl geiser-guile magit php-mode markdown-mode haskell-mode undo-tree evil auctex pdf-tools))
+   '(auctex command-log-mode company-coq evil geiser-guile haskell-mode
+	    impatient-mode magit markdown-mode merlin nodejs-repl
+	    pdf-tools php-mode proof-general rainbow-delimiters tuareg
+	    undo-tree web-mode))
  '(proof-assistants '(coq))
  '(proof-prog-name "coqtop")
  '(proof-prog-name-guess t)
  '(show-paren-mode t)
+ '(tool-bar-mode nil)
  '(warning-suppress-log-types '((comp))))
 
 (add-hook 'TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'coq-mode-hook 'undo-tree-mode)
+(add-hook 'js-mode-hook
+             (lambda ()
+                (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-expression)
+                (define-key js-mode-map (kbd "C-c C-j") 'nodejs-repl-send-line)
+                (define-key js-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
+                (define-key js-mode-map (kbd "C-c C-c") 'nodejs-repl-send-buffer)
+                (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
+                (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
 
 (require 'evil)
 (evil-mode t)
@@ -59,14 +69,6 @@
 
 (fido-mode)
 
-(add-hook 'js-mode-hook
-             (lambda ()
-                (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-expression)
-                (define-key js-mode-map (kbd "C-c C-j") 'nodejs-repl-send-line)
-                (define-key js-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
-                (define-key js-mode-map (kbd "C-c C-c") 'nodejs-repl-send-buffer)
-                (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
-                (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
 
 
 ;; (use-package company-coq
@@ -79,6 +81,5 @@
 ;;   )
  ;; (unbind-key "<menu>" company-coq-map)
 
-(add-hook 'coq-mode-hook 'undo-tree-mode)
 ; (add-hook 'company-coq--keybindings-minor-mode '(fido-mode))
 	  ;(lambda () (unbind-key "<menu>" company-coq-map)))
